40ffc9544956080ae3cd881ad39f5c5d4952e057
Muhammad Usman Ali Nasir <usman.nasir@A006-00819.local>
2025-01-23 20:42:54 +0500

    feat(login-page): added login functionality for session-portal and setup store

---
+++ src/redux/store/configureStore.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+import { baseApi } from "../baseApi";
+
+const rootReducer = combineReducers({
+  [baseApi.reducerPath]: baseApi.reducer,
+});
+
+const store = configureStore({
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([baseApi.middleware]),
+  reducer: rootReducer,
+});
+
+export { store };
