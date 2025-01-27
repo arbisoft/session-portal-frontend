a645cd01a63c45d1c1cb1b0a8dc26c8b67341ddf
ahmad-arbisoft <ahmad.habib@arbisoft.com>
2025-01-27 20:15:07 +0500

    chore: file convention fix

---
+++ src/components/containers/MainLayoutContainer/mainLayoutContainer.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import { usePathname } from "next/navigation";
+import {
+  MainContainer,
+  MainStack,
+  SidebarContainer,
+  RightBarContainer,
+  ContentContainer,
+} from "./styled";
+
+type TMainLayoutContainer = {
+  children: React.ReactNode;
+};
+
+const MainLayoutContainer = ({ children }: TMainLayoutContainer) => {
+  const pathname = usePathname();
+  const showRightBar = pathname.includes("/");
+
+  return (
+    <MainContainer>
+      <MainStack>
+        <SidebarContainer></SidebarContainer>
+
+        <ContentContainer>{children}</ContentContainer>
+
+        {showRightBar && (
+          <RightBarContainer>
+            <h3>Suggested for You</h3>
+          </RightBarContainer>
+        )}
+      </MainStack>
+    </MainContainer>
+  );
+};
+
+export default MainLayoutContainer;
