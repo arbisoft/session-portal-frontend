eb6690abbefc8a0384880ca9c8abf1f50378a246
ahmad-arbisoft <ahmad.habib@arbisoft.com>
2025-01-27 12:56:18 +0500

    feat: remove styled components package and use mui for styling

---
+++ src/components/containers/MainLayoutContainer/styled.tsx
@@ -0,0 +1,51 @@
+import { styled, css } from "@mui/material/styles";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+
+export const MainContainer = styled(Container, {
+  name: "MainContainer",
+})(() => {
+  return css`
+    height: 100vh;
+  `;
+});
+
+export const MainStack = styled(Stack, {
+  name: "MainStack",
+})(() => {
+  return css`
+    flex-direction: row;
+    height: 100%;
+  `;
+});
+
+export const SidebarContainer = styled(Box, {
+  name: "SidebarContainer",
+})(() => {
+  return css`
+    height: 100%;
+    width: 220px;
+    border: 1px solid #ddd;
+  `;
+});
+
+export const ContentContainer = styled(Box, {
+  name: "ContentContainer",
+})(() => {
+  return css`
+    flex-grow: 1;
+    height: 100%;
+    padding: 2px;
+  `;
+});
+
+export const RightBarContainer = styled(Box, {
+  name: "RightBarContainer",
+})(() => {
+  return css`
+    height: 100%;
+    width: 230px;
+    border: 1px solid #ddd;
+  `;
+});
