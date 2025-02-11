b62600e52d42e28e7c7b8aca921345bb45885634
ahmad-arbisoft <ahmad.habib@arbisoft.com>
2025-02-11 16:39:21 +0500

    feat: video listing page - video card

---
+++ src/features/VideosPage/videosPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import MainLayoutContainer from "@/components/containers/MainLayoutContainer";
+import VideoCard from "@/components/VideoCard";
+
+const VideosPage = () => {
+  return (
+    <MainLayoutContainer>
+      <VideoCard />
+    </MainLayoutContainer>
+  );
+};
+
+export default VideosPage;
