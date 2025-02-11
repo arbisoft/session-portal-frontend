bfb173425f92680eb69b93c2f81fecbb317c88fb
Qaisar Irfan <qaisar.irfan@arbisoft.com>
2025-02-11 17:43:13 +0500

    refactor: remove notistack from project

--- src/components/AlertModal/types.ts
+++
@@ -1,7 +0,0 @@
-export type AlertModalProps = {
-  handleCloseAlertModal: () => void;
-  errorMessage?: string;
-  vertical?: "bottom" | "top";
-  horizontal?: "center" | "left" | "right";
-  severity?: "success" | "info" | "warning" | "error";
-};
