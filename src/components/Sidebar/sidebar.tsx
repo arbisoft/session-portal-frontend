41b3ea8f8e799cc2294d0d7559b78912911b0bbf
ahmad-arbisoft <ahmad.habib@arbisoft.com>
2025-02-19 12:57:51 +0500

    feat: add api integration in sidebar

--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -1,30 +1,51 @@
 import React, { useState } from "react";
 
+import { faker } from "@faker-js/faker";
+import Box from "@mui/material/Box";
+import Skeleton from "@mui/material/Skeleton";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 import useSidebar from "@/hooks/useSidebar";
+import { Tag } from "@/models/Events";
+import useLanguage from "@/services/i18n/use-language";
 
 import { MenuItem, MenuStack, SidebarContainer, Text } from "./styled";
 import { SidebarProps } from "./types";
 
+const loadingTags: string[] = Array(5)
+  .fill("")
+  .map(() => faker.lorem.words(1));
+
 const Sidebar = ({ handleSidebarToggle }: SidebarProps) => {
-  const { sidebarItems } = useSidebar();
+  const router = useRouter();
+  const language = useLanguage();
+  const { sidebarItems, isDataLoading } = useSidebar();
+
   const [selected, setSelected] = useState("");
 
-  const handleClick = (item: string) => {
-    setSelected(item);
+  const handleClick = (item: Tag) => {
+    setSelected(item.name);
     handleSidebarToggle?.();
+    router.push(item.id === 0 ? `/${language}/videos` : `/${language}/videos?tag=${item.id}`);
   };
 
   return (
     <SidebarContainer>
       <MenuStack>
-        {sidebarItems.map((item) => (
-          <MenuItem key={item} isSelected={item === selected} onClick={() => handleClick(item)}>
-            <Image src="/assets/images/sidebar-item-icon.svg" alt={item} width={18} height={12} />
-            <Text>{item}</Text>
-          </MenuItem>
-        ))}
+        {isDataLoading
+          ? loadingTags?.map((item) => (
+              <Box key={item} display="flex" justifyContent="space-between" alignItems={"center"} width="90%">
+                <Skeleton variant="rounded" width="20%" height={30} />
+                <Skeleton width="76%" height={25} />
+              </Box>
+            ))
+          : sidebarItems.map((item) => (
+              <MenuItem key={item.id} isSelected={item.name === selected} onClick={() => handleClick(item)}>
+                <Image src="/assets/images/sidebar-item-icon.svg" alt={item.name} width={18} height={12} />
+                <Text>{item.name}</Text>
+              </MenuItem>
+            ))}
       </MenuStack>
     </SidebarContainer>
   );
