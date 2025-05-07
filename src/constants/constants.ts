234466df67666ab6723c5ed8b336fb28f85c2163
ahmad-arbisoft <ahmad.habib@arbisoft.com>
2025-05-07 13:18:11 +0500

    feat: remove language from url, add in localstorage & add favicon

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,5 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? "";
 export const DEFAULT_THUMBNAIL = "/assets/images/temp-youtube-logo.webp";
+
+export const DEFAULT_LANGUAGE = "en";
+export const DEFAULT_LANGUAGE_DIR = "ltr";
