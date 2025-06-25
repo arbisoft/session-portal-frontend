c9abf84f578cce712aab5290028c160838d62efb
Shaheer Alam <shaheer.alam@arbisoft.com>
2025-06-25 13:55:04 +0500

    186 Integrate Hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 import { NotificationProvider } from "@/components/Notification";
 import InitColorSchemeScript from "@/components/theme/init-color-scheme-script";
@@ -23,6 +24,22 @@ export default function RootLayout({ children }: { children: React.ReactNode; pa
       <head>
         <meta name="apple-mobile-web-app-title" content="Arbisoft Session Portal" />
         <GoogleTagManager gtmId={gtmId} />
+        <Script
+          id="hotjar-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:6412285,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </head>
       <body>
         <GoogleOAuthProvider clientId={clientId}>
