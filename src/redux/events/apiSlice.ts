2996f1c7c52d27a392a9260a6fb4e3db2bc5c2ca
Shaheer Alam <shaheer.alam@arbisoft.com>
2025-07-04 15:40:58 +0500

    feat: integrate pagination with infinite scroll for recommendations

--- src/redux/events/apiSlice.ts
+++ src/redux/events/apiSlice.ts
@@ -1,6 +1,6 @@
 import isEqual from "lodash/isEqual";
 
-import { EventDetail, Tag, AllEventResponse, EventsParams, Recommendation, Playlist } from "@/models/Events";
+import { EventDetail, Tag, AllEventResponse, EventsParams, Recommendation, Playlist, RecommendationParam } from "@/models/Events";
 import { baseApi } from "@/redux/baseApi";
 
 export const eventsApi = baseApi.injectEndpoints({
@@ -54,11 +54,26 @@ export const eventsApi = baseApi.injectEndpoints({
         method: "GET",
       }),
     }),
-    recommendation: builder.query<Recommendation[], string>({
-      query: (id) => ({
+    recommendation: builder.query<Recommendation, RecommendationParam>({
+      query: ({ id, page = 1, page_size = 10 }) => ({
         url: `/events/recommendations/${id}/`,
         method: "GET",
+        params: { page, page_size },
       }),
+      serializeQueryArgs: ({ endpointName, queryArgs }) => `${endpointName}-${queryArgs.id}`,
+      merge: (currentCache, newItems) => {
+        currentCache.count = newItems.count;
+        currentCache.previous = newItems.previous;
+        currentCache.next = newItems.next;
+
+        const existingIds = new Set(currentCache.results.map((event) => event.id));
+        const uniqueResults = newItems.results.filter((event) => !existingIds.has(event.id));
+
+        currentCache.results.push(...uniqueResults);
+      },
+      forceRefetch({ currentArg, previousArg }) {
+        return !isEqual(currentArg, previousArg);
+      },
     }),
     playlists: builder.query<Playlist[], void>({
       query: () => ({
