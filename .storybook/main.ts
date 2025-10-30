dedfee1d4eb4400c53acc7b5edbd6266a485e629
Qaisar Irfan <qaisar.irfan@arbisoft.com>
2025-10-30 20:08:57 +0500

    chore(deps): upgrade Next.js to v15 and React to v19

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-  ],
+  addons: ["@storybook/addon-onboarding", "@storybook/addon-links", "@storybook/addon-essentials"],
   framework: {
     name: "@storybook/nextjs",
     options: {},
