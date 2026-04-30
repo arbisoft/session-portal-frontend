696bb54d6d762e2523d2bd9e71d84dda57b04a3a
Usman Younas <usman.younas@arbisoft.com>
2026-04-30 18:24:29 +0500

    add captions support and view counting

--- src/components/VideoPlayer/types.ts
+++ src/components/VideoPlayer/types.ts
@@ -1,14 +1,17 @@
 import { MediaCrossOrigin } from "@vidstack/react";
 
 export interface VideoPlayerProps {
+  captionsFile?: string;
   className?: string;
   crossOrigin?: true | MediaCrossOrigin | null;
   height?: string;
   onVideoEnded?: VoidFunction;
+  onWatchThreshold?: () => void;
   playsInline?: boolean;
   posterAlt: string;
   posterSrc: string;
   title: string;
   videoSrc: string;
+  watchThresholdSeconds?: number;
   width?: string;
 }
